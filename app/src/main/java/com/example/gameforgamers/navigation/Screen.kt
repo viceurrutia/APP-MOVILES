7aa9a9dd0d999a67271c7cbb741706188275ad63
vicenteurrutia <vice.urrutia@duocuc.cl>
2025-10-27 03:01:29 -0300

    se elimina el proyecto anterior

--- app/src/main/java/com/example/gameforgamers/navigation/Screen.kt
+++
@@ -1,7 +0,0 @@
-package com.example.gameforgamers.navigation
-
-sealed class Screen(val route: String) {
-    object Catalogo : Screen("catalogo")
-    object Pedido : Screen("pedido") // Renombrado
-    // Añade Login, etc. aquí
-}
