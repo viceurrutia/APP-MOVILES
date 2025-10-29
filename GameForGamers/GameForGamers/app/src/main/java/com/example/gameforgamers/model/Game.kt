0806e6a2a614f79e023bdff68ea7c3b2a94be0f2
vicenteurrutia <vice.urrutia@duocuc.cl>
2025-10-29 05:12:28 -0300

    eliminar gameforgamers

--- GameForGamers/GameForGamers/app/src/main/java/com/example/gameforgamers/model/Game.kt
+++
@@ -1,3 +0,0 @@
-package com.example.gameforgamers.model
-import java.io.Serializable
-data class Game(val id:Int, val title:String, val price:String, val drawableName:String, val description:String): Serializable
